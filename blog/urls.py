8451f63d970b56f61bc6d75cf3f695a7e7fb49ed
dimicoding <>
2023-04-11 10:34:38 +0000

    Install authentication to the blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+#from . import views
+from .views import HomeView, FirstRecipeView
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    # path('home/', views.home, name='home'),
+    path('home/', HomeView.as_view(), name="home"),
+    path('recipe/<int:pk>', FirstRecipeView.as_view(), name="first_recipe"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.template import loader
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 
-def home(request):
-    return render(request, 'base.html', {})
+#def home(request):
+#   return render(request, 'base.html', {})
+
+
+class HomeView(ListView):
+    model = Recipe
+    template_name = "base.html"
+    
+
+class FirstRecipeView(DetailView):
+    model = Recipe
+    template_name = "first-recipe.html"
