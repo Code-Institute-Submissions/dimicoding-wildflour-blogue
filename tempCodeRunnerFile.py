63ab4b09ed1d92a18537bc002c6ecbb6f5d0ff3e
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-04-26 11:07:17 +0000

    Style the ListView, remove the border from bootstrap card class

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
 print("Hello world")
+                    {% if user.is_authenticated % }
+                        {% if user.id == recipe.author.id % }
+                           <button class = "btn btn-success" >
+                                <a href = "{% url 'edit_recipe' recipe.pk %}" > My Recipe < /a >
+                            </button >
+                        { % endif % }
+                    {% endif %}
