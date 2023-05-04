8acde17ce3c12401b8bd025673d231a16bfaef5f
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-05-04 14:38:46 +0000

    Change console to smtp in the setting.py

--- baking/settings.py
+++ baking/settings.py
@@ -185,7 +185,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
